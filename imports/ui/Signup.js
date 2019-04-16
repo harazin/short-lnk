03846ccc69aaa8618aec6ebecc63049ad19dcc6c
M.Harazin <mh757@kent.ac.uk>
2019-04-16 07:56:16 +0100

    [6:57] - Added accounts-password package.

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {Accounts} from 'meteor/accounts-base';
 
 export default class Signup extends React.Component {
 	constructor(props) {
@@ -12,8 +13,11 @@ export default class Signup extends React.Component {
 	onSubmit(e) {
 		e.preventDefault();
 
-		this.setState({
-			error: 'Something went wrong!'
+		let email = this.refs.email.value.trim();
+		let password = this.refs.password.trim();
+
+		Accounts.createUser({email: {email}, password: {password}},(err) => {
+			console.log('Signup callback', err);
 		});
 	}
 
@@ -25,8 +29,8 @@ export default class Signup extends React.Component {
 			{this.state.error ? <p>Error: {this.state.error}</p> :  undefined}
 
 			<form onSubmit={this.onSubmit.bind(this)}>
-				<input type="email" name="email" placeholder="Email" />
-				<input type="password" name="password" placeholder="Password" />
+				<input type="email" ref="email" name="email" placeholder="Email" />
+				<input type="password" ref="password" name="password" placeholder="Password" />
 				<button>Create Account</button>
 			</form>
 
