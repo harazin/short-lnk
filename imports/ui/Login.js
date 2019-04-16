db78ee97db4d33beabad1fb4626d8dfaf3f12890
M.Harazin <mh757@kent.ac.uk>
2019-04-16 12:37:03 +0100

    [6:58] - Added Login function to Login page.

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,13 +1,38 @@
+import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import {Link} from 'react-router';
 
 export default class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: ''
+		};
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+
+		let email = this.refs.email.value.trim();
+		let password = this.refs.password.value.trim();
+
+		Meteor.loginWithPassword({email}, password, (err) => {
+			console.log('Login callback', err);
+		});
+	}
+
   render() {
     return (
 			<div>
-				<h1>Login to Short Lnk</h1>
+				<h1>Short Lnk</h1>
 
-				Login from here.
+				{this.state.error ? <p>{this.state.error}</p> : undefined}
+
+				<form onSubmit={this.onSubmit.bind(this)}>
+					<input type="email" ref="email" name="email" placeholder="Email"/>
+					<input type="password" ref="password" name="password" placeholder="Password"/>
+					<button>Log In</button>
+				</form>
 
 				<Link to="/signup">Sign up</Link>
 			</div>
