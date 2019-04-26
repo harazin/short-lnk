449d188c4bd41f6e86c653fefa66b3e98f333210
M.Harazin <mh757@kent.ac.uk>
2019-04-26 10:05:45 +0100

    [6:58] - Matched files to course files.

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
-import {Meteor} from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
@@ -9,12 +9,12 @@ import NotFound from '../imports/ui/NotFound';
 import Login from '../imports/ui/Login';
 
 const routes = (
-	<Router history={browserHistory}>
-		<Route path="/" component={Login}/>
-		<Route path="/signup" component={Signup} />
-		<Route path="/links" component={Link} />
-		<Route path="*" component={NotFound}/>
-	</Router>
+  <Router history={browserHistory}>
+    <Route path="/" component={Login}/>
+    <Route path="/signup" component={Signup}/>
+    <Route path="/links" component={Link}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
 );
 
 Meteor.startup(() => {
--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,41 +1,39 @@
-import {Meteor} from 'meteor/meteor';
 import React from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
+import { Meteor } from 'meteor/meteor';
 
 export default class Login extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: ''
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+  }
+  onSubmit(e) {
+    e.preventDefault();
 
-	onSubmit(e) {
-		e.preventDefault();
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
 
-		let email = this.refs.email.value.trim();
-		let password = this.refs.password.value.trim();
-
-		Meteor.loginWithPassword({email}, password, (err) => {
-			console.log('Login callback', err);
-		});
-	}
-
+    Meteor.loginWithPassword({email}, password, (err) => {
+      console.log('Login callback', err);
+    });
+  }
   render() {
     return (
-			<div>
-				<h1>Short Lnk</h1>
+      <div>
+        <h1>Short Lnk</h1>
 
-				{this.state.error ? <p>{this.state.error}</p> : undefined}
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-				<form onSubmit={this.onSubmit.bind(this)}>
-					<input type="email" ref="email" name="email" placeholder="Email"/>
-					<input type="password" ref="password" name="password" placeholder="Password"/>
-					<button>Log In</button>
-				</form>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Email"/>
+          <input type="password" ref="password" name="password" placeholder="Password"/>
+          <button>Login</button>
+        </form>
 
-				<Link to="/signup">Sign up</Link>
-			</div>
-		);
+        <Link to="/signup">Have an account?</Link>
+      </div>
+    );
   }
 }
