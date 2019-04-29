3df118df371a4b5184ff2ab02444a35d28955a70
M.Harazin <mh757@kent.ac.uk>
2019-04-29 13:09:23 +0100

    [6:62] - Played around with schema navigation.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,26 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	// const employeeSchema = new SimpleSchema({
+	// 	name: {
+	// 		type: String,
+	// 		min: 1,
+	// 		max: 200,
+	// 	},
+	// 	hourlyWage: {
+	// 		type: Number,
+	// 		min: 0
+	// 	},
+	// 	email: {
+	// 		type: String,
+	// 		regEx: SimpleSchema.RegEx.Email,
+	// 	}
+	// });
+	//
+	// employeeSchema.validate({
+	// 	name: 'Mark',
+	// 	hourlyWage: 6,
+	// 	email: "Mark@gmail.com"
+	// });
 });
