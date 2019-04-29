3577a5e600de5a0567835a49c30498ca62876c98
M.Harazin <mh757@kent.ac.uk>
2019-04-29 16:41:00 +0100

    [7:68] - Added Links database and added "Add Link" feature to /links.

--- client/main.js
+++ client/main.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 
 import { routes, onAuthChange } from '../imports/routes/routes';
+import { Links } from '../imports/api/links';
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
+});
+
+Tracker.autorun(() => {
+	Links.find().fetch().map((link) => {
+		console.log("Link: ", link.url);
+	});
 });
 
 Meteor.startup(() => {
