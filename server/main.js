bdb0860438cea0fff566b0080d59125f263a3241
M.Harazin <mh757@kent.ac.uk>
2019-04-29 13:25:19 +0100

    [6:63] - Added Accounts.validateNewUser to server.

--- server/main.js
+++ server/main.js
@@ -1,7 +1,25 @@
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
+	Accounts.validateNewUser((user) => {
+		const email = user.emails[0].address;
+	try {
+		new SimpleSchema({
+			email: {
+				type: String,
+				regEx: SimpleSchema.RegEx.Email,
+			}
+		}).validate({ email });
+	} catch (e) {
+		throw new Meteor.Error(400, e.message);
+	}
+
+	return true;
+});
+
+
 	// const employeeSchema = new SimpleSchema({
 	// 	name: {
 	// 		type: String,
