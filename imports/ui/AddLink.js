3ff428a25f5eaf6c75288db4128b7f2e04b2f582
M.Harazin <mh757@kent.ac.uk>
2019-05-05 21:14:19 +0100

    [8:87] - Turned Add Link and hide/unhide checkbox into controlled inputs.

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -2,25 +2,43 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 
 export default class AddLink extends React.Component {
-	onSubmit(e){
-		const url = this.refs.url.value.trim();
-		e.preventDefault();
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    };
+  }
+  onSubmit(e) {
+    const { url } = this.state;
 
-		if(url){
-			Meteor.call('links.insert', url);
-			this.refs.url.value = '';
-		}
-	}
+    e.preventDefault();
 
-	render() {
-		return (
-			<div>
-				<p>Add Link</p>
-				<form onSubmit={this.onSubmit.bind(this)}>
-					<input type="text" ref="url" placeholder="URL" />
-					<button>Add Link</button>
-				</form>
-			</div>
-		)
-	}
+    if (url) {
+      Meteor.call('links.insert', url, (err, res) => {
+        if (!err) {
+          this.setState({ url: '' });
+        }
+      });
+    }
+  }
+  onChange(e) {
+    this.setState({
+      url: e.target.value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <p>Add Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+            <input
+              type="text"
+              placeholder="URL"
+              value={this.state.url}
+              onChange={this.onChange.bind(this)}/>
+            <button>Add Link</button>
+        </form>
+      </div>
+    );
+  }
 }
