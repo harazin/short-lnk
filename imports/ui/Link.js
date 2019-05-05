ae0c6a358f4a8700974854c39c55c2dc5e246457
M.Harazin <mh757@kent.ac.uk>
2019-05-05 20:44:10 +0100

    [8:86] - Added checkbox to toggle showing hidden links.

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import PrivateHeader from './PrivateHeader';
 import LinksList from './LinksList';
+import LinksListFilters from './LinksListFilters';
 import AddLink from './AddLink';
 
 export default () => {
   return (
     <div>
       <PrivateHeader title="Your Links"/>
+			<LinksListFilters/>
       <LinksList/>
       <AddLink/>
     </div>
