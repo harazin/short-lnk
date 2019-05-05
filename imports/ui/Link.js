ae2f0b0f05dc4245efeb256d56c0f435204534ca
M.Harazin <mh757@kent.ac.uk>
2019-05-05 13:51:29 +0100

    [8:79] - Refactored Link page to have individual react components in it.

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,36 +1,16 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
 
-import { Links } from '../api/links';
+import PrivateHeader from './PrivateHeader';
 import LinksList from '../ui/linksList';
+import AddLink from './AddLink';
 
 export default class Link extends React.Component {
-  onLogout() {
-    Accounts.logout();
-  }
-	onSubmit(e){
-		const url = this.refs.url.value.trim();
-		e.preventDefault();
-
-		if(url){
-			Meteor.call('links.insert', url);
-			this.refs.url.value = '';
-		}
-	}
   render() {
     return (
       <div>
-        <h1>Your Links</h1>
-        <button onClick={this.onLogout.bind(this)}>Logout</button>
-
+				<PrivateHeader title="Your Links" />
 				<LinksList />
-
-				<p>Add Link</p>
-				<form onSubmit={this.onSubmit.bind(this)}>
-					<input type="text" ref="url" placeholder="URL" />
-					<button>Add Link</button>
-				</form>
+				<AddLink />
       </div>
     );
   }
