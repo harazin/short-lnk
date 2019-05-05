7f3d72cd7ee154074a762772ee110909844cab72
M.Harazin <mh757@kent.ac.uk>
2019-05-05 14:09:57 +0100

    [8:80] - Converted viable react components to stateless components.

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -4,14 +4,12 @@ import PrivateHeader from './PrivateHeader';
 import LinksList from '../ui/linksList';
 import AddLink from './AddLink';
 
-export default class Link extends React.Component {
-  render() {
-    return (
-      <div>
-				<PrivateHeader title="Your Links" />
-				<LinksList />
-				<AddLink />
-      </div>
-    );
-  }
-}
+export default () => {
+	return (
+		<div>
+			<PrivateHeader title="Your Links" />
+			<LinksList />
+			<AddLink />
+		</div>
+	);
+};
