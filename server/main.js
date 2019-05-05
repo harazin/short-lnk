8dbb9ac126c891201d34e34337c61d78a5db4bf9
M.Harazin <mh757@kent.ac.uk>
2019-05-05 08:59:14 +0100

    [7:75] - Played around with node HTTP and connectHandlers.

--- server/main.js
+++ server/main.js
@@ -1,9 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';
 import '../imports/api/links';
 import '../imports/startup/simple-schema-configuration';
 
 Meteor.startup(() => {
+	WebApp.connectHandlers.use((req, res, next) => {
+		console.log('This is a middleware!');
+		console.log(req.url, req.method, req.headers, req.query);
 
+		// res.statusCode = 404;
+		//
+		// res.setHeader('my-custom-header', 'Max was here!');
+		// red.write('<h1>This is a middleware!</h1>');
+		// red.end();
+		next();
+	});
 });
