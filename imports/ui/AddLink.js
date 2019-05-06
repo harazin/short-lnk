88e346b1a503ec5ef7ab67e5bad6dce9ba860121
M.Harazin <mh757@kent.ac.uk>
2019-05-06 18:16:12 +0100

    [9:97] - Reused style modifiers and added primary color variables.

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -45,19 +45,21 @@ export default class AddLink extends React.Component {
           contentLabel="Add link"
           onAfterOpen={() => this.refs.url.focus()}
           onRequestClose={this.handleModalClose.bind(this)}
-					ariaHideApp={false}>
+					ariaHideApp={false}
+					className="boxed-view__box"
+					overlayClassName="boxed-view boxed-view--modal">
           <h1>Add Link</h1>
           {this.state.error ? <p>{this.state.error}</p> : undefined}
-          <form onSubmit={this.onSubmit.bind(this)}>
+          <form onSubmit={this.onSubmit.bind(this)} className="boxed-view__form">
               <input
                 type="text"
                 placeholder="URL"
                 ref="url"
                 value={this.state.url}
                 onChange={this.onChange.bind(this)}/>
-              <button>Add Link</button>
+              <button className="button">Add Link</button>
+							<button className="button button--secondary" type="button" onClick={this.handleModalClose.bind(this)}>Cancel</button>
           </form>
-          <button onClick={this.handleModalClose.bind(this)}>Cancel</button>
         </Modal>
       </div>
     );
